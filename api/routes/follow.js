404d1529b89be3c0f9f94d1e77dab24ed16b50b0
Janapar <mjgvalencia@gmail.com>
2018-06-27 09:20:18 +0200

    Parte Back

---
+++ api/routes/follow.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var express = require('express');
+var FollowController = require('../controllers/follow');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+//api.get('/pruebas', md_auth.ensureAuth, FollowController.pruebas2);
+api.post('/follow',md_auth.ensureAuth ,FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+
+module.exports = api;
