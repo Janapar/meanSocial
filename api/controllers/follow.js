75eb686870f3074d3da1fee23e7ffbb16703fade
Janapar <mjgvalencia@gmail.com>
2018-06-27 14:52:21 +0200

    modificaciones red social

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -38,8 +38,33 @@ function deleteFollow(req, res){
   });
 }
 
+function getFollowingUsers(req, res){
+  var userId = req.user.sub;
+  if(req.params.id){
+    userId = req.params.id;
+  }
+  var page = 1;
+  if(req.params.page){
+      page = req.params.page;
+  }
+
+  var itemsPerPage = 4;
+
+  Follow.find({user:userId}).populate({path:'followed'}).paginate(page, itemsPerPage, (err, follows, total) =>{
+    if(err) return res.status(500).send({message:'Error al dejar de seguir'});
+    if(!follows) return res.status(404).send({message: 'No esta siguiendo ningun follow'});
+    return res.status(200).send({
+        total: total,
+        pages: Math.ceil(total/itemPerPage),
+        follows
+    });
+  });
+
+}
+
 module.exports = {
   //pruebas2
   saveFollow,
-  deleteFollow
+  deleteFollow,
+  getFollowingUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 //api.get('/pruebas', md_auth.ensureAuth, FollowController.pruebas2);
 api.post('/follow',md_auth.ensureAuth ,FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
 
 module.exports = api;
