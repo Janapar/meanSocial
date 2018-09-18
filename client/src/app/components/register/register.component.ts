f17932de166236baf84b9f92f1b50658c0aa2c8c
Janapar <mjgvalencia@gmail.com>
2018-09-18 12:08:49 +0200

    subida 18/09

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Observable} from 'rxjs/Observable';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-register',
@@ -18,14 +20,34 @@ export class RegisterComponent implements OnInit {
     private _userService: UserService
   ) {
     this.title = 'Registrate';
-    this.user = new User('','','','','','','ROLE_USER','');
+    this.user = new User("",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "ROLE_USER",
+    "");
   }
 
   ngOnInit() {
     console.log('componente register cargado...');
   }
+
   onSubmit(){
-    console.log(this.user);
-    this._userService.register;
+    //this._userService.register(this.user));
+  this._userService.register(this.user).subscribe(
+      response => {
+        console.log(response.user);
+        if(response.user && response.user._id){
+         alert('dentro del If');
+          console.log(response.user);
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+
   }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,18 +1,26 @@
 import { Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs';
 import { GLOBAL } from './global';
 import { User } from '../models/user';
 
 
 @Injectable()
 export class UserService{
-  public url:string;
+  public url: string;
 
   constructor(public _http: HttpClient){
     this.url = GLOBAL.url;
   }
-  register(){
-    console.log(this.url);
+//register(user_to_register){
+ //  console.log(user_to_register);
+//  console.log(this.url);
+//}
+  register(user: User): Observable<any> {
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    console.log(params);
+    return this._http.post(this.url+'register', params, {headers:headers});
+
   }
 }
